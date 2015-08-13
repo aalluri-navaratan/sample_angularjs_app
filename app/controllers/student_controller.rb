01d704177672f9cdfad4fd764a5a5643c62d9979
Alluri Anil kumar <alluri.anil190@gmail.com>
2015-08-13 18:54:53 +0530

    added the fist controller view file

---
+++ app/controllers/student_controller.rb
@@ -0,0 +1,19 @@
+class StudentController < ApplicationController
+	
+	def index
+		#render :text => " aneel"
+	end
+
+	def create	
+	end
+	
+	def show
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+end
